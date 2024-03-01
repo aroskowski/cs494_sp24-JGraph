0ab7ba5d83802865674368750084c0b79821566d
Anika K Roskowski <aroskows@tesla21.eecs.utk.edu>
2024-02-29 20:20:52 -0500

    updated UI and .jgr creation

--- ui.py
+++ ui.py
@@ -2,11 +2,27 @@
 import sys
 import re 
 
+
 #=============================================================================
-#                       Print Functions
+#                           Globals
+#=============================================================================
+jgr = open("floorplan.jgr", "w")
+jgr.write("newgraph\nxaxis nodraw\nyaxis nodraw\n\n")
+pts = []
+roompts = []
+#=============================================================================
+#                       JGraph Interaction Functions
 #=============================================================================
 def print_external(points):
-    print("newline linethickness 2 color 0 0 0 " + points);
+    jgr.write("newline linethickness 2 color 0 0 0 pts\n")
+    for i in points:
+        jgr.write(f"{i[0]} {i[1]}\n")
+    jgr.write("\n")
+def print_internal(roompts):
+    jgr.write("newline linethickness 2 color .3 .3 .3 pts\n")
+    for i in roompts:
+        jgr.write(f"{i[0]} {i[1]}\n")
+    jgr.write("\n")
 #=============================================================================
 #                       Calculation Functions
 #=============================================================================
@@ -32,8 +48,8 @@ def perimeter(org, uin):
         points.append((x,y)); #add to list of points
 
     if(points[len(points) - 1] != points[0]):
-        return False;
-    return True;
+        return (False, points);
+    return (True, points);
 
 #=============================================================================
 #                       Main Function
@@ -41,6 +57,19 @@ def perimeter(org, uin):
 org = input("Enter the orgin point (x y): ");
 org = org.split(" ");
 uin = input("Enter Exterior Dimensions: ");
-if(not perimeter(org, uin)):
+test, pts = perimeter(org, uin)
+if(not test):
     print("Invalid input.\nEnter directions to walk the perimeter of the building using n (north), e (east), s (south), or w (west) followed by the number of feet to continue in this direction before another turn.\n", file=sys.stderr);
+print_external(pts);
+
+num = int(input("Enter number of rooms: "));
+for i in range(num):
+    org = input("Enter the orgin point for the room (x y): ");
+    org = org.split(" ");
+    uin = input("Enter Interior Dimesions: ");
+    test, roompts = perimeter(org, uin);
+    if (not test):
+        print("Room error. pts did not line up", file=sys.stderr);
+    print_internal(roompts);
+
 
